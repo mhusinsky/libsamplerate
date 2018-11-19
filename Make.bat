d6e0993077ed9872b9100ec9c72c0c2b9553b904
Matthias Husinsky <mhusinsky@fhstp.ac.at>
2018-11-19 17:58:21 +0100

    added "lib" as build target to makefile and batch file

--- Make.bat
+++ Make.bat
@@ -2,6 +2,7 @@
 
 if "%1"=="check" GOTO CHECK
 if "%1"=="clean" GOTO CLEAN
+if "%1"=="lib" GOTO LIB
 
 copy /y Win32\config.h src\config.h
 copy /y Win32\unistd.h examples\unistd.h
@@ -16,6 +17,10 @@ goto END
 
 :CLEAN
 nmake -f Win32\Makefile.msvc clean
+goto END
+
+:LIB
+nmake -f Win32\Makefile.msvc lib
 goto END
 
 :END
